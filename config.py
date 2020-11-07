f4e2368fbf5ae71211d08f45e3e66ffe1188ccff
Tihomir S <tihomir.saulic@gmail.com>
2020-11-07 22:08:33 +1000

    Bump version

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os, semver
 
-version = semver.VersionInfo.parse('0.1.0-pre.2+build.5')
+version = semver.VersionInfo.parse('0.1.1-pre.2+build.6')
 game_db_path = 'sqlite:///../game.sqlite'
 secret_key = os.environ.get("DARKNOVA_SECRET_KEY") or "test"
