73edd9bd1a877a84ac7bb36d7484e0839428396a
tsaulic <tihomir.saulic@gmail.com>
2020-11-11 18:00:37 +1000

    Fix errors for PostgreSQL

--- core/routes/populate.py
+++ core/routes/populate.py
@@ -1,14 +1,12 @@
 from random import randrange
 
 import flask
-from flask import request, Blueprint, redirect, url_for
+from flask import request, Blueprint
 from sqlalchemy import exc
 
 from configuration import sectors_default_amount
 from core import db
-from core.models import Planet
-from core.models import Player
-from core.models import Sector
+from core.models import Player, Planet, Sector
 
 bp = Blueprint('populate', __name__)
 
@@ -19,7 +17,8 @@ def populate_mock_db(sectors_value):
     db.drop_all()
     db.create_all()
 
-    db.session.add(sol)
+    commit_try()
+
     db.session.add(Player(
         username="Admin",
         email='admin@example.com',
@@ -27,25 +26,15 @@ def populate_mock_db(sectors_value):
         sector=sol
     ))
 
+    commit_try()
+
     for sector in range(1, sectors_value):
         db.session.add(Sector(id=sector, name=''))
         if has_planet(4) and sector != 0: db.session.add(Planet(name='Unowned', sector_id=sector))
         if has_planet(2) and sector != 0: db.session.add(Planet(name='Unowned', sector_id=sector))
         if has_planet(0) and sector != 0: db.session.add(Planet(name='Unowned', sector_id=sector))
 
-        try:
-            db.session.commit()
-        except AssertionError as err:
-            db.session.rollback()
-            flask.abort(409, err)
-        except exc.IntegrityError as err:
-            db.session.rollback()
-            flask.abort(409, err.orig)
-        except Exception as err:
-            db.session.rollback()
-            flask.abort(500, err)
-        finally:
-            db.session.close()
+    commit_try()
 
 
 def has_planet(cutoff):
@@ -62,20 +51,7 @@ def insert_player(player_name, ship_name):
         sector=sol
     ))
 
-    try:
-        db.session.commit()
-    except AssertionError as err:
-        db.session.rollback()
-        flask.abort(409, err)
-    except exc.IntegrityError as err:
-        db.session.rollback()
-        flask.abort(409, err.orig)
-    except Exception as err:
-        db.session.rollback()
-        flask.abort(500, err)
-    finally:
-        db.session.close()
-        return redirect(url_for('play.play'))
+    commit_try()
 
 
 @bp.route('/populate', methods=['GET'])
@@ -105,3 +81,19 @@ def populate():
         flask.abort(400, 'DB already created')
 
     return 'success'
+
+
+def commit_try():
+    try:
+        db.session.commit()
+    except AssertionError as err:
+        db.session.rollback()
+        flask.abort(409, err)
+    except exc.IntegrityError as err:
+        db.session.rollback()
+        flask.abort(409, err.orig)
+    except Exception as err:
+        db.session.rollback()
+        flask.abort(500, err)
+    finally:
+        db.session.close()
