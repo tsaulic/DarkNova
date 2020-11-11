ccbe8847868535ee995306c01b655c53987e6ba2
tsaulic <tihomir.saulic@gmail.com>
2020-11-11 11:33:27 +1000

    Fix CSS and add logout button

--- configuration.py
+++ configuration.py
@@ -2,7 +2,7 @@ import os
 
 import semver
 
-version = semver.VersionInfo.parse('0.2.4-alpha+build.12')
+version = semver.VersionInfo.parse('0.2.4-alpha+build.13')
 # game_db_path = 'sqlite:///../game.sqlite'
 game_db_path = 'sqlite:///:memory:'
 secret_key = os.environ.get("DARKNOVA_SECRET_KEY") or "test"
