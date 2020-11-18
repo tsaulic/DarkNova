cc3f138d1ba322046f0b7ad5d33d11ec61a89933
tsaulic <tihomir.saulic@gmail.com>
2020-11-18 17:23:18 +1000

    Small refactor of CSS and migrade_prod.py tweak

--- migrate_prod.py
+++ migrate_prod.py
@@ -5,8 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 # set up connection to your Google Cloud SQL DB instance
 # python migrate_prod.py db upgrade; after you've created your migration files
+from configuration import system
 
-game_db_path = ''
+if system == 'Linux':
+    game_db_path = '' # fix prod path and certs
+else:
+    game_db_path = 'postgres+psycopg2://postgres:password@localhost:5432/darknova' # use local DB
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = game_db_path
